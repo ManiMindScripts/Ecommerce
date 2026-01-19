b8374821d8dcb10cf00dca88dc4f7e718ebbad52
Akhuwat <Akhuwat@L-MIS-015>
2026-01-19 10:56:56 +0500

    Changes

---
+++ Ecommerce/Controllers/StockController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Authorize(Roles = nameof(Roles.Admin))]
+    public class StockController : Controller
+    {
+        private readonly IStockRepo _stockRepo;
+
+        public StockController(IStockRepo stockRepo)
+        {
+            _stockRepo = stockRepo;
+        }
+        public async Task<IActionResult> Index(string sTerm ="")
+        {
+            var stocks = await _stockRepo.GetStocks(sTerm);
+            return View(stocks);
+        }
+    }
+}
